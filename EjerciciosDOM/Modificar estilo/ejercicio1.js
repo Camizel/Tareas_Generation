a7f4211e0b280b57a1d86e5c21fc40aca4b278c8
Camila Muñoz <Munozp.camila98@gmail.com>
2023-06-27 12:46:07 -0400

    ejercicios martes

---
+++ EjerciciosDOM/Modificar estilo/ejercicio1.js
@@ -0,0 +1,13 @@
+// Modificar el estilo del texto del párrafo mediante código javascript. Ejemplo de archivo HTML:
+// Al hacer clic en el botón, la fuente, el tamaño de fuente y el color del texto del párrafo deben cambiar.
+const parrafo = document.querySelector('#text')
+const btn = document.querySelector('#jsstyle')
+
+btn.addEventListener('click', cambiarEstilos)
+
+function cambiarEstilos(){
+  parrafo.style.fontFamily = 'Lucida Sans Regular'
+  parrafo.style.fontWeigh = 'bold'
+  parrafo.style.fontSize = '20px'
+  parrafo.style.color = 'violet';
+}
